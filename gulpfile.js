6622edd59800bad2eff99e45fe938be4d43dde99
taromsoft <rt05@ya.ru>
2020-07-10 19:28:03 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
